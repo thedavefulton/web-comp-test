052c2d9ea7048032ca495d9a7627e992c66df273
David Fulton <david.fulton@lifestylepubs.com>
2022-01-13 20:41:34 -0600

    make adding components less suicide-inducing

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
-import {defineCustomElement} from "vue";
-import Banner from "./components/Banner.ce.vue";
-import BannerChild from "./components/BannerChild.ce.vue"
-import Unrelated from "./components/Unrelated.ce.vue"
+import { defineCustomElement } from "vue";
+import _kebabCase from "lodash/kebabCase";
 
-export function register() {
-    const BannerCE = defineCustomElement(Banner);
-    const BannerChildCE = defineCustomElement(BannerChild);
-    const UnrelatedCE = defineCustomElement(Unrelated)
-
-    customElements.define('my-banner', BannerCE);
-    customElements.define('my-banner-child', BannerChildCE);
-    customElements.define('my-unrelated', UnrelatedCE);
+const components = [
+  "Banner",
+  "BannerChild",
+  "Unrelated",
+  "BaseCard",
+  "TitleCard",
+];
+export async function register() {
+  for (const compName of components) {
+    const element = `ark-${_kebabCase(compName)}`;
+    const comp = await import(`./components/${compName}.ce.vue`);
+    customElements.define(element, defineCustomElement(comp.default));
+  }
 }
 
-register();
+await register();
