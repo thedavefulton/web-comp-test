b4a040e0260b85d37cf578513975306b10a1f1ba
David Fulton <david.fulton@lifestylepubs.com>
2022-01-13 20:40:34 -0600

    added cards

---
+++ src/components/BaseCard.ce.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="base-card">
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+:host(.base-card) {
+  color: var(--gray-0);
+}
+.base-card {
+  border-radius: var(--radius-3);
+  border: var(--border-size-1) solid var(--gray-1);
+  max-width: var(--size-15);
+  padding: var(--size-5);
+  background-color: var(--blue-5);
+  color: var(--gray-0);
+}
+</style>
---
+++ src/components/TitleCard.ce.vue
@@ -0,0 +1,14 @@
+<template>
+  <ark-base-card>
+    <div class="title">
+      <slot name="title"></slot>
+    </div>
+    <slot></slot>
+  </ark-base-card>
+</template>
+
+<style>
+.title {
+  margin-bottom: var(--size-3);
+}
+</style>
