b58af7aa0d81675a0518aea1ca0c7100e535754b
David Fulton <david.fulton@lifestylepubs.com>
2022-01-13 20:41:18 -0600

    tinker and prettier

--- src/components/Banner.ce.vue
+++ src/components/Banner.ce.vue
@@ -1,34 +1,36 @@
 <template>
-  <h1><slot>Well Hello There</slot> {{formattedName}}!</h1>
-  <input type="text" v-model="name" />
+  <ark-base-card>
+    <h1><slot>Well Hello There</slot> {{ formattedName }}!</h1>
+    <input type="text" v-model="name" />
 
-  <my-banner-child :name="formattedName"></my-banner-child>
+    <ark-banner-child :name="formattedName"></ark-banner-child>
+  </ark-base-card>
 </template>
 
 <script setup lang="ts">
-import {computed, ref} from "vue";
-import {userStore as useUserStore} from "../stores";
+import { computed, ref } from "vue";
+import { userStore as useUserStore } from "../stores";
 
 const props = defineProps({
-  upper: Boolean
-})
+  upper: Boolean,
+});
 
 const userStore = useUserStore();
-console.log(userStore.name)
+console.log(userStore.name);
 
 const name = computed({
   get: () => userStore.name,
   set: (val) => {
     userStore.name = val;
-  }
-})
+  },
+});
 
-
-
-const formattedName = computed(() => props.upper ? name.value.toLocaleUpperCase() : name.value)
+const formattedName = computed(() =>
+  props.upper ? name.value.toLocaleUpperCase() : name.value
+);
 </script>
 
-<style scoped>
+<style>
 h1 {
   font-size: 2rem;
 }
--- src/components/Unrelated.ce.vue
+++ src/components/Unrelated.ce.vue
@@ -1,23 +1,21 @@
 <template>
-  <h2>I'm My Own Thing!</h2>
-  <input type="text" v-model="name" />
+  <ark-base-card>
+    <h2>I'm My Own Thing!</h2>
+    <input type="text" v-model="name" />
 
-  <my-banner-child :name="name"></my-banner-child>
+    <ark-banner-child :name="name"></ark-banner-child>
+  </ark-base-card>
 </template>
 
 <script setup lang="ts">
-import {userStore as useUserStore} from "../stores";
-import {computed} from "vue";
+import { userStore as useUserStore } from "../stores";
+import { computed } from "vue";
 
 const userStore = useUserStore();
 const name = computed({
   get: () => userStore.name,
   set: (val) => {
-    userStore.name = val
-  }
-})
+    userStore.name = val;
+  },
+});
 </script>
-
-<style scoped>
-
-</style>
